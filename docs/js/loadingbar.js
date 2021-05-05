dc614c1d7b062c2b82e47bfc13433bb7670ce782
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2021-05-05 12:56:47 -0400

    Update loadingbar.js

--- docs/js/loadingbar.js
+++ docs/js/loadingbar.js
@@ -1,19 +1,20 @@
 var i = 0;
+var add = 5;
 function move() {
   if (i == 0) {
     i = 1;
     var elem = document.getElementById("myBar");
     var textupdate = document.getElementById("myProgress");
-    var width = 10;
+    var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 100) {
+      if (width >= 95) {
         clearInterval(id);
         i = 0;
       } else {
         width++;
         elem.style.width = width + "%";
-        textupdate.innerHTML = width  + "%";
+        textupdate.innerHTML = width + add + "% This Can Take A Few Minutes";
       }
     }
   }
